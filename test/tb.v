f305820a46f665995a9471e0062dd2fa302704c5
Jordan Fernandez <109761575+jf-894@users.noreply.github.com>
2026-08-18 11:07:57 -0700

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -29,7 +29,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_UART_3FIR user_project (
+  tt_um_UART_3FIR (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
