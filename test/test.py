bff113c8e208503a681f5176c7a093c01df1f7e1
Jordan Fernandez <109761575+jf-894@users.noreply.github.com>
2026-08-19 12:00:55 -0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -10,20 +10,20 @@ from cocotb.triggers import Timer, Edge
 async def uart_tx(dut, data):
     # Start bit (0)
     dut.ui_in.value = 0
-    await Timer(104167, units='ns')
+    await Timer(104167, unit='ns')
     
     # Send 8 data bits (LSB first)
     for i in range(8):
         bit_val = (data >> i) & 1
         dut.ui_in.value = bit_val
-        await Timer(104167, units='ns')
+        await Timer(104167, unit='ns')
         
     # Stop bit (1)
     dut.ui_in.value = 1
-    await Timer(104167, units='ns')
+    await Timer(104167, unit='ns')
     
     # Delay between data bytes (500us)
-    await Timer(500000, units='ns')
+    await Timer(500000, unit='ns')
 
 # Manual UART RX function for 9600 baud
 async def uart_rx(dut):
@@ -41,14 +41,14 @@ async def uart_rx(dut):
     # 3. Sample the 8 data bits (LSB first)
     for i in range(8):
         # Wait a full bit period (1 / 9600 sec)
-        await Timer(104167, units='ns')
+        await Timer(104167, unit='ns')
         
         # Read the current bit value of uo_out[0] (the last character in the binstr)
         bit_val = int(dut.uo_out.value.binstr[-1])
         received_byte |= (bit_val << i)
         
     # 4. Wait a full bit period to reach the center of the stop bit
-    await Timer(104167, units='ns')
+    await Timer(104167, unit='ns')
     
     return received_byte
 
@@ -57,7 +57,7 @@ async def test_project(dut):
     dut._log.info("Start Simulation")
 
     # 15.15ns period = ~66 MHz clock
-    clock = Clock(dut.clk, 15.15, units="ns")
+    clock = Clock(dut.clk, 15.15, unit="ns")
     cocotb.start_soon(clock.start())
 
     # Initial values
@@ -68,10 +68,10 @@ async def test_project(dut):
 
     # Reset
     dut._log.info("Applying Reset")
-    await Timer(100, units='ns')
+    await Timer(100, unit='ns')
     dut.rst_n.value = 1
     
-    await Timer(10000, units='ns')
+    await Timer(10000, unit='ns')
 
     # Send Samples
     dut._log.info("Sending Samples...")
