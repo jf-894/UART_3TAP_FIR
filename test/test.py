529158daac50eaefa8eba9ae1c6bdc11f57f3b40
Jordan Fernandez <109761575+jf-894@users.noreply.github.com>
2026-08-19 12:16:26 -0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -5,27 +5,26 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import Timer, Edge
 
-
-# Manual UART TX function for 9600 baud
+# Manual UART TX function for 1 Mbps baud (Fast Simulation)
 async def uart_tx(dut, data):
     # Start bit (0)
     dut.ui_in.value = 0
-    await Timer(104167, unit='ns')
+    await Timer(1000, unit='ns')
     
     # Send 8 data bits (LSB first)
     for i in range(8):
         bit_val = (data >> i) & 1
         dut.ui_in.value = bit_val
-        await Timer(104167, unit='ns')
+        await Timer(1000, unit='ns')
         
     # Stop bit (1)
     dut.ui_in.value = 1
-    await Timer(104167, unit='ns')
+    await Timer(1000, unit='ns')
     
-    # Delay between data bytes (500us)
-    await Timer(500000, unit='ns')
+    # Reduced delay between data bytes (5us instead of 500us)
+    await Timer(5000, unit='ns')
 
-# Manual UART RX function for 9600 baud
+# Manual UART RX function for 1 Mbps baud (Fast Simulation)
 async def uart_rx(dut):
     # 1. Wait for the start bit (bit 0 drops from 1 to 0)
     while True:
@@ -34,21 +33,21 @@ async def uart_rx(dut):
             break
             
     # 2. Wait half a bit period to sample in the middle of the start bit
-    await Timer(52083, units='ns')
+    await Timer(500, unit='ns')
     
     received_byte = 0
     
     # 3. Sample the 8 data bits (LSB first)
     for i in range(8):
-        # Wait a full bit period (1 / 9600 sec)
-        await Timer(104167, unit='ns')
+        # Wait a full bit period (1 / 1_000_000 sec)
+        await Timer(1000, unit='ns')
         
         # Read the current bit value of uo_out[0] (the last character in the binstr)
         bit_val = int(dut.uo_out.value.binstr[-1])
         received_byte |= (bit_val << i)
         
     # 4. Wait a full bit period to reach the center of the stop bit
-    await Timer(104167, unit='ns')
+    await Timer(1000, unit='ns')
     
     return received_byte
 
